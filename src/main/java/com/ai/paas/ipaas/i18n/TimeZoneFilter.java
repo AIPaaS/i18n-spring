b4df8489c1e572d99d01bf4116d04fdfefe9e4d2
DOUXF <douxf@asiainfo.com>
2016-10-29 16:29:48 +0800

    add timezone surpport

---
+++ src/main/java/com/ai/paas/ipaas/i18n/TimeZoneFilter.java
@@ -0,0 +1,67 @@
+package com.ai.paas.ipaas.i18n;
+
+import java.io.IOException;
+import java.util.TimeZone;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.context.i18n.LocaleContextHolder;
+
+/**
+ * @author DOUXF 用于探测用户的时区，全部拦截，在首页时尾部增加一段js代码，然后发送到固定的URL,
+ *         需要下面的js来实现 Date.prototype.stdTimezoneOffset = function() { var jan =
+ *         new Date(this.getFullYear(), 0, 1); var jul = new
+ *         Date(this.getFullYear(), 6, 1); return
+ *         Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset()); }
+ * 
+ *         Date.prototype.dst = function() { return this.getTimezoneOffset() <
+ *         this.stdTimezoneOffset(); } today.stdTimezoneOffset();
+ *         需要想方法将stdTimezoneOffset发送过来
+ */
+public class TimeZoneFilter implements Filter {
+
+	private final String USER_TIME_ZONE = "USER_TIME_ZONE";
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		String offset = request.getParameter("offset");
+		if (null != offset && !"".equals(offset)) {
+			int timeOffset = Integer.parseInt(offset);
+			timeOffset = (0 - timeOffset) / 60;
+			LocaleContextHolder.setTimeZone(TimeZone.getTimeZone("GMT+"
+					+ timeOffset));
+			// 放到session里面
+			HttpServletRequest httpRequest = (HttpServletRequest) request;
+			httpRequest.getSession(true).setAttribute(USER_TIME_ZONE,
+					"GMT+" + timeOffset);
+		} else {
+			// 看来得放到session里面
+			HttpServletRequest httpRequest = (HttpServletRequest) request;
+			HttpSession session = httpRequest.getSession(false);
+			if (null != session && null != session.getAttribute(USER_TIME_ZONE)) {
+				LocaleContextHolder.setTimeZone(TimeZone
+						.getTimeZone("GMT+"+(String) session
+								.getAttribute(USER_TIME_ZONE)));
+			}
+		}
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+	}
+
+}
