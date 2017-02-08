125313590797d1eae07e6ae735f712d4b2245918
douxiaofeng <douxf@asiainfo.com>
2017-02-08 10:17:13 +0800

    upgrade version

--- src/main/java/com/ai/paas/ipaas/i18n/TimeZoneFilter.java
+++ src/main/java/com/ai/paas/ipaas/i18n/TimeZoneFilter.java
@@ -11,6 +11,9 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * @author DOUXF 用于探测用户的时区，全部拦截，在首页时尾部增加一段js代码，然后发送到固定的URL, 需要下面的js来实现
  *         Date.prototype.stdTimezoneOffset = function() { var jan = new
@@ -23,6 +26,7 @@ import javax.servlet.http.HttpSession;
  *         需要想方法将stdTimezoneOffset发送过来
  */
 public class TimeZoneFilter implements Filter {
+	private static final Logger log = LogManager.getLogger(TimeZoneFilter.class.getName());
 
 	private final String USER_TIME_ZONE = "USER_TIME_ZONE";
 
@@ -32,13 +36,15 @@ public class TimeZoneFilter implements Filter {
 	}
 
 	@Override
-	public void doFilter(ServletRequest request, ServletResponse response,
-			FilterChain chain) throws IOException, ServletException {
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
 		if (!(request instanceof HttpServletRequest)) {
 			chain.doFilter(request, response);
 			return;
 		}
 		String offset = request.getParameter("offset");
+		if (log.isInfoEnabled())
+			log.info("------The user zone offset:" + offset);
 		if (null != offset && !"".equals(offset)) {
 			int timeOffset = Integer.parseInt(offset);
 			timeOffset = (0 - timeOffset) / 60;
@@ -46,21 +52,23 @@ public class TimeZoneFilter implements Filter {
 				ZoneContextHolder.setZone("GMT+" + timeOffset);
 			else
 				ZoneContextHolder.setZone("GMT" + timeOffset);
+			if (log.isInfoEnabled())
+				log.info("------The user zone time offset:" + ZoneContextHolder.getZone());
 			// 放到session里面
 			HttpServletRequest httpRequest = (HttpServletRequest) request;
-			httpRequest.getSession(true)
-					.setAttribute(
-							USER_TIME_ZONE,
-							(timeOffset >= 0 ? "GMT+" + timeOffset : "GMT"
-									+ timeOffset));
+			httpRequest.getSession(true).setAttribute(USER_TIME_ZONE,
+					(timeOffset >= 0 ? "GMT+" + timeOffset : "GMT" + timeOffset));
 		} else {
+			if (log.isInfoEnabled())
+				log.info("------Need to set user zone time offset!....");
 			// 看来得放到session里面
 			HttpServletRequest httpRequest = (HttpServletRequest) request;
 			HttpSession session = httpRequest.getSession(false);
 			if (null != session && null != session.getAttribute(USER_TIME_ZONE)) {
-				ZoneContextHolder.setZone((String) session
-						.getAttribute(USER_TIME_ZONE));
+				ZoneContextHolder.setZone((String) session.getAttribute(USER_TIME_ZONE));
 			}
+			if (log.isInfoEnabled())
+				log.info("------The user zone time offset after set:" + ZoneContextHolder.getZone());
 		}
 		chain.doFilter(request, response);
 	}
